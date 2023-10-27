59bc5ac5b52e87d1e1c280eb61b0a5dd1923edca
Younra <85308154+Younra@users.noreply.github.com>
2023-10-27 17:38:51 +0200

    Update fxmanifest.lua

--- younra_halloween/fxmanifest.lua
+++ younra_halloween/fxmanifest.lua
@@ -3,7 +3,7 @@ game "rdr3"
 
 this_is_a_map "yes"
 rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
-name "halloween"
+name "younra_halloween"
 files {
     'stream/doll.ytyp',
     'stream/ouija.ytyp',
