e08a17eab025b8dfeba7402692590afec098f0b3
Younra <85308154+Younra@users.noreply.github.com>
2023-10-27 17:37:31 +0200

    Add files via upload

---
+++ younra_halloween/fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version "adamant"
+game "rdr3"
+
+this_is_a_map "yes"
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+name "halloween"
+files {
+    'stream/doll.ytyp',
+    'stream/ouija.ytyp',
+    'stream/pumpkin.ytyp',
+    'stream/pumpkin2.ytyp',
+    'stream/pumpkin3.ytyp',
+    'stream/pumpkin4.ytyp',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/doll.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/ouija.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/pumpkin.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/pumpkin2.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/pumpkin3.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/pumpkin4.ytyp'
+  
+escrow_ignore {
+    "stream/*.ymap",
+    "stream/*.ydr",
+    "stream/*.ytd",
+    "stream/*.ytyp"
+}  
